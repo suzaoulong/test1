3f653c63f66ba20eebb909a196de324a6e634f58
suzaoulong <130963630+suzaoulong@users.noreply.github.com>
2023-05-29 15:09:56 +0800

    Add files via upload

---
+++ test2/test/src/script.js
@@ -0,0 +1,8 @@
+document.write("HELLO ")
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count++;
+      counterElement.textContent = count;
+    }
